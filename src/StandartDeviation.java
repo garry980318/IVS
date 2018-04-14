78d5f5d6d72cc39b07112a8ee6f0d54339a6edb6
Ondra Holub <ondrejholub202@gmail.com>
2018-04-14 15:51:52 +0200

    Add files via upload

---
+++ src/StandartDeviation.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class StandartDeviation{
+
+    public static void main(String[] args) {
+
+        IMath mathLib = new Math();
+    
+        Scanner inputLine = new Scanner(System.in);
+        String line = inputLine.nextLine();
+        Scanner input = new Scanner(line);
+    
+        double number = 0;
+        double sumOfNumbers = 0;
+        double sumOfSquaredNumbers = 0;
+        double countOfNumbers = 0;
+
+
+        while(input.hasNextDouble()){
+            number = input.nextDouble();
+            sumOfNumbers = mathLib.Sum(sumOfNumbers, number);
+            sumOfSquaredNumbers = mathLib.Sum(sumOfSquaredNumbers, mathLib.Pow(number, 2));
+            countOfNumbers++; //countOfNumbers = mathlib.Sum(countOfNumbers, 1)???
+        }
+
+        double mean = mathLib.Div(sumOfNumbers, countOfNumbers);
+        double result = mathLib.Root(mathLib.Div(mathLib.Sub(sumOfSquaredNumbers, mathLib.Mult(countOfNumbers, mathLib.Pow(mean, 2))),mathLib.Sub(countOfNumbers,1)));
+
+        System.out.println("standart dev is " + result);
+
+    }
+
+}
