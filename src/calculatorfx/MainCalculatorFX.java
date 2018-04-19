18509ce9f5b5fb54acb4bc6ef9f824a275677ef9
garry980318 <radoslav.grencik@gmail.com>
2018-04-19 17:19:42 +0200

    Style.css moved to folder Styles. Added doxy for Controller.java and MainCalculatorFX.java

--- src/calculatorfx/MainCalculatorFX.java
+++ src/calculatorfx/MainCalculatorFX.java
@@ -1,26 +1,39 @@
+/**
+ * This file contains a java implementation of a main class for a java FXML application. It is a part of a group project for the subject IVS of Brno University of Technology. 
+ *
+ * @file MainCalculatorFX.java
+ * @author Rene Bolf & Radoslav Grencik
+ * @brief Implementation of a main class for a java FXML application.
+ * @package calculatorfx
+ */
 package calculatorfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainCalculatorFX extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        
         Parent root = FXMLLoader.load(getClass().getResource("CalcFX.fxml"));
-        
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/calculatorfx/Style.css");
-        stage.setTitle("DedSec Calculator");
-        
+        scene.getStylesheets().add("/styles/Style.css"); /**< Added custom css styling */
+        stage.setTitle("DedSec Calculator"); /**< Added title */
+        Image image = new Image("/icons/icon.png"); /**< Added custom icon */
+        stage.getIcons().add(image);        
         stage.setScene(scene);
         stage.show();
+        
     }
     
     public static void main(String[] args) {
         launch(args);
     }    
 }
+
+/*** END OF FILE MainCalculatorFX.java ***/
